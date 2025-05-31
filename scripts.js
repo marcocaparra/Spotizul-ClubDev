003f688d29734fcdbd3acb24eeaea1d43edc3db6
Marco Caparra <marcocaparra.profissional@gmail.com>
2025-05-31 19:58:26 -0300

    Primeiro commit do projeto Spotify

---
+++ scripts.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const artistsData = [
+    { name: "Henrique e Juliano", image: "./img/artista-henrique-juliano.jpg" },
+    { name: "Jorge & Mateus", image: "./img/artista-jorge-mateus.jpg" },
+    { name: "Zé Neto & Cristiano", image: "./img/artista-ze-neto.jpg" },
+    { name: "Gusttavo Lima", image: "./img/artista-gusttavo-lima.jpg" },
+    { name: "Luan Santana", image: "./img/artista-luan-santana.jpg" },
+    { name: "Mateus & Kauan", image: "./img/artista-mateus-kauan.jpg" }
+];
+    const albumsData = [
+    { name: "White Noise (Sleep & Relaxation Sounds)", artist: "Sleepy John", image: "./img/album-white-noise.jpg" },
+    { name: "O Céu Explica Tudo (Ao Vivo)", artist: "Henrique & Juliano", image: "./img/album-ceu-explica.jpg" },
+    { name: "Vida Loka", artist: "Racionais", image: "./img/album-vida-loka.jpg" },
+    { name: "HIT ME HARD AND SOFT", artist: "Billie Eilish", image: "./img/album-hit-me.jpg" },
+    { name: "CAJU", artist: "Liniker", image: "./img/album-caju.jpg" },
+    { name: "Escândalo Íntimo", artist: "Luísa Sonza", image: "./img/album-escandalo.jpg" }
+];
+
+    const artistsGrid = document.querySelector(".artists-grid");
+    const albumsGrid = document.querySelector(".albums-grid");
+
+    artistsData.forEach(artist => {
+        const artistCard = document.createElement("div");
+        artistCard.classList.add("artist-card");
+
+        artistCard.innerHTML = `
+            <img src="${artist.image}" alt="image do ${artist.name}">
+            <div>
+                <h3>${artist.name}</h3>
+                <p>Artista</p>
+            </div>
+        `
+
+        artistsGrid.appendChild(artistCard)
+    })
+    albumsData.forEach(album => {
+        const albumCard = document.createElement("div");
+        albumCard.classList.add("album-card");
+
+        albumCard.innerHTML = `
+            <img src="${album.image}" alt="imagem do álbum ${album.name}">
+            <div>
+                <h3>${album.name}</h3>
+                <p>${album.artist}</p>
+            </div>
+        `
+
+        albumsGrid.appendChild(albumCard)
+    })
+})
